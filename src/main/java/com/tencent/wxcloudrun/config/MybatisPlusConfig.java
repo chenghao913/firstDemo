8c41c60ebfd830805a323e272666a2865dc1fe12
Administrator <haome520@msn.cn>
2025-02-16 01:45:28 +0800

    更改  增加debug信息

--- src/main/java/com/tencent/wxcloudrun/config/MybatisPlusConfig.java
+++ src/main/java/com/tencent/wxcloudrun/config/MybatisPlusConfig.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.Configuration;
 import java.time.LocalDateTime;
 
 @Configuration
-@MapperScan("com.tencent.wxcloudrun.dao")
 public class MybatisPlusConfig {
 
     /**
